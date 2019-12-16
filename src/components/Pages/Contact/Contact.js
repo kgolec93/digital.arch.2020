d00818086983072b6fd694d60cfb0ac31c2b08ec
kgolec93 <kamil.golec@gmail.com>
2019-12-16 23:17:56 +0100

    Contact page with contact form created (no backend)

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Banner from '../../Elements/Banner'
 import './Contact.scss'
 import * as Scroll from 'react-scroll';
 import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import banner04 from '../../../assets/img/banner04.jpg'
+import logo from '../../../assets/img/digitalarch-logo.png'
 
 
-export const Contact = () => {
-    return (
-        <div className='contactContainer'>
-            <Banner section="Skontaktuj się z nami" button='Czytaj dalej' bgImage={banner04} />
-            <p>Contact page</p>
-        </div>
-    )
+
+export class Contact extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className='contactContainer'>
+                <Banner section="Skontaktuj się z nami" button='Czytaj dalej' bgImage={banner04} />
+                <div className="contactContent">
+                    <div>
+                        <img src={logo} alt="digitalarch-logo" />
+                        <p>info@digitalarch.pl</p>
+                        <p>+48 783 648 000</p>
+                    </div>
+                    <div>
+                        <form onSubmit={(e) => { e.preventDefault() }}>
+                        <h1>Napisz do nas</h1>
+
+                            <input type="email" placeholder='Email' id='email'/>
+
+                            <input type="text" placeholder='Twoje imię' id='name'/>
+
+                            <input type="text" placeholder='Temat wiadomości' id='topic'/>
+
+                            <textarea ref='textArea' type="text" placeholder='Twoja wiadomość' id='text'/>
+                            <button type="submit">
+                                Wyślij wiadomość
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }

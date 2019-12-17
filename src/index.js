9d5d1d8249978d07e8f0e498320e8fb718258e6e
kgolec93 <kamil.golec@gmail.com>
2019-12-17 07:13:07 +0100

    React snapshot implemented

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom'
 
-ReactDOM.render(
+render(
 <Router>
     <App />
 </Router>

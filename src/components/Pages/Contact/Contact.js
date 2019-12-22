dedf0948d6277b6bc758715c21ac18506005ad6d
kgolec93 <kamil.golec@gmail.com>
2019-12-22 21:46:35 +0100

    Contact form backend implemented

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -6,16 +6,57 @@ import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } f
 import banner04 from '../../../assets/img/banner04.jpg'
 import logo from '../../../assets/img/digitalarch-logo.png'
 import { Helmet } from 'react-helmet'
-
+import $ from 'jquery'
+import ReCAPTCHA from "react-google-recaptcha";
 
 
 export class Contact extends Component {
     constructor() {
         super();
         this.state = {
-
+            isVerified: false,
+            formEmail: '',
+            formName: '',
+            formSubject: '',
+            formMessage: '',
+            alertMessage: ''
         }
     }
+
+    submitForm = (e) => {
+        e.preventDefault();
+        if (this.state.formEmail === '') {
+            this.setState({ alertMessage: 'Musisz podać swój email!' })
+        }
+        else {
+            if (this.state.formName === '') {
+                this.setState({ alertMessage: 'Podaj swoje imię!' })
+            }
+            else {
+                if (this.state.formMessage === '') {
+                    this.setState({ alertMessage: 'Pole wiadomości nie może być puste!' })
+                }
+                else {
+                    if (this.state.isVerified === true) {
+                        $.ajax({
+                            data: this.state,
+                            type: 'POST',
+                            url: '/api/contact.php',
+                            success: (data) => {
+                                console.info(data)
+                                this.setState({ formName: '', formEmail: '', formSubject: '', formMessage: '', alertMessage: data })
+                            },
+                            error: (xhr, status, err) => {
+                                console.error(status, err.toString())
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    }
+
+
 
     render() {
         return (
@@ -33,16 +74,47 @@ export class Contact extends Component {
                         <p>+48 783 648 000</p>
                     </div>
                     <div>
-                        <form onSubmit={(e) => { e.preventDefault() }}>
+                        <form onSubmit={this.submitForm}>
                             <h1>Napisz do nas</h1>
+                            <p>{this.state.alertMessage}</p>
+                            <input
+                                type="email"
+                                placeholder='Email'
+                                onChange={(e) => this.setState({ formEmail: e.target.value })}
+                                value={this.state.formEmail}
+                                id='email'
+                            />
 
-                            <input type="email" placeholder='Email' id='email' />
+                            <input
+                                type="text"
+                                onChange={(e) => this.setState({ formName: e.target.value })}
+                                value={this.state.formName}
+                                placeholder='Twoje imię'
+                                id='name'
 
-                            <input type="text" placeholder='Twoje imię' id='name' />
+                            />
 
-                            <input type="text" placeholder='Temat wiadomości' id='topic' />
+                            <input
+                                type="text"
+                                onChange={(e) => this.setState({ formSubject: e.target.value })}
+                                value={this.state.formSubject}
+                                placeholder='Temat wiadomości'
+                                id='topic'
+                            />
 
-                            <textarea ref='textArea' type="text" placeholder='Twoja wiadomość' id='text' />
+                            <textarea
+                                ref='textArea'
+                                type="text"
+                                placeholder='Twoja wiadomość'
+                                id='text'
+                                onChange={(e) => this.setState({ formMessage: e.target.value })}
+                                value={this.state.formMessage}
+                            />
+                            <ReCAPTCHA
+                                onChange={() => { this.setState({ isVerified: !this.state.isVerified }) }}
+                                theme='dark'
+                                sitekey="6LdQSskUAAAAANBdxt7_P_dB4mcmt6Wxa2SF8cTG"
+                            />
                             <button type="submit">
                                 Wyślij wiadomość
                             </button>

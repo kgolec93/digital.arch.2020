6ebfdbc257b6b29692666415607fbefa7d95b122
kgolec93 <kamil.golec@gmail.com>
2019-12-23 11:09:08 +0100

    Portfolio HQ images added

---
+++ src/components/Pages/Portfolio/data.js
@@ -0,0 +1,229 @@
+import img01 from '../../../assets/img/portfolio/01.jpg'
+import img02 from '../../../assets/img/portfolio/02.jpg'
+import img03 from '../../../assets/img/portfolio/03.jpg'
+import img04 from '../../../assets/img/portfolio/04.jpg'
+import img05 from '../../../assets/img/portfolio/05.jpg'
+import img06 from '../../../assets/img/portfolio/06.jpg'
+import img07 from '../../../assets/img/portfolio/07.jpg'
+import img08 from '../../../assets/img/portfolio/08.jpg'
+import img09 from '../../../assets/img/portfolio/09.jpg'
+import img10 from '../../../assets/img/portfolio/10.jpg'
+import img11 from '../../../assets/img/portfolio/11.jpg'
+import img12 from '../../../assets/img/portfolio/12.jpg'
+import img13 from '../../../assets/img/portfolio/13.jpg'
+import img14 from '../../../assets/img/portfolio/14.jpg'
+import img15 from '../../../assets/img/portfolio/15.jpg'
+import img16 from '../../../assets/img/portfolio/16.jpg'
+import img17 from '../../../assets/img/portfolio/17.jpg'
+import img18 from '../../../assets/img/portfolio/18.jpg'
+import img19 from '../../../assets/img/portfolio/19.jpg'
+import img20 from '../../../assets/img/portfolio/20.jpg'
+import img21 from '../../../assets/img/portfolio/21.jpg'
+import img22 from '../../../assets/img/portfolio/22.jpg'
+import img23 from '../../../assets/img/portfolio/23.jpg'
+import img24 from '../../../assets/img/portfolio/24.jpg'
+import img25 from '../../../assets/img/portfolio/25.jpg'
+import img26 from '../../../assets/img/portfolio/26.jpg'
+import img27 from '../../../assets/img/portfolio/27.jpg'
+import img28 from '../../../assets/img/portfolio/28.jpg'
+import img29 from '../../../assets/img/portfolio/29.jpg'
+import img30 from '../../../assets/img/portfolio/30.jpg'
+import img31 from '../../../assets/img/portfolio/31.jpg'
+import img32 from '../../../assets/img/portfolio/32.jpg'
+import img33 from '../../../assets/img/portfolio/33.jpg'
+import img34 from '../../../assets/img/portfolio/34.jpg'
+import img35 from '../../../assets/img/portfolio/35.jpg'
+import img36 from '../../../assets/img/portfolio/36.jpg'
+import img37 from '../../../assets/img/portfolio/37.jpg'
+
+
+export const images = [
+    
+    {
+        url: img01,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img02,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img03,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img04,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img05,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img06,
+        alt: 'alt1',
+        type: 'special',
+    },
+    {
+        url: img07,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img08,
+        alt: 'alt1',
+        type: 'product',
+    },
+    {
+        url: img09,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img10,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img11,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img12,
+        alt: 'alt1',
+        type: 'product',
+    },
+    {
+        url: img13,
+        alt: 'alt1',
+        type: 'special',
+    },
+    {
+        url: img14,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img15,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img16,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img17,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img18,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img19,
+        alt: 'alt1',
+        type: 'product',
+    },
+    {
+        url: img20,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img21,
+        alt: 'alt1',
+        type: 'product',
+    },
+    {
+        url: img22,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img23,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img24,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img25,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img26,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img27,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img28,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img29,
+        alt: 'alt1',
+        type: 'special',
+    },
+    {
+        url: img30,
+        alt: 'alt1',
+        type: 'product',
+    },
+    {
+        url: img31,
+        alt: 'alt1',
+        type: 'interior',
+    },
+    {
+        url: img32,
+        alt: 'alt1',
+        type: 'product',
+    },
+    {
+        url: img33,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img34,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img35,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img36,
+        alt: 'alt1',
+        type: 'arch',
+    },
+    {
+        url: img37,
+        alt: 'alt1',
+        type: 'interior',
+    },
+]
+
+export default images

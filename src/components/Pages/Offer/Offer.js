8c2611befd25ca81c79785029600a9720bffa20f
kgolec93 <kamil.golec@gmail.com>
2019-12-23 10:19:05 +0100

    Offer HQ images added

--- src/components/Pages/Offer/Offer.js
+++ src/components/Pages/Offer/Offer.js
@@ -5,21 +5,28 @@ import './Offer.scss'
 import OfferSection from './OfferSection'
 import { Helmet } from 'react-helmet'
 
+import ARCH from '../../../assets/img/offer/01_ARCH.jpg'
+import INT from '../../../assets/img/offer/02_INT.jpg'
+import PRODUCT from '../../../assets/img/offer/03_PRODUCT.jpg'
+import VR from '../../../assets/img/offer/04_VR.jpg'
+import GRAPHICS from '../../../assets/img/offer/05_GRAPHICS.jpg'
+
+
 export const sections = [
     {
         name: 'Wizualizacje architektoniczne',
         refName: 'wizArchi',
-        img: 'https://www.digitalarch.pl/static/media/53.9074f059.jpg',
+        img: ARCH,
         description: `Naszą specjalnością są wizualizacje architektoniczne dla pracowni projektowych, deweloperów oraz projektantów wnętrz. Gdy jednak wszystko idzie w stronę fotorealizmu, my poszliśmy o krok dalej opracowując unikatowy styl graficzny, który udało się nam zyskać dzięki zastosowaniu najnowszych technologii oraz odrobiny magii. Nasze wizualizacje wyróżnią Cię na tle konkurencji, ułatwiając sprzedanie projektu, czy wygranie ważnego konkursu. 
         Nie ogranicza nas również forma. Standardowo nasze wizualizacje są tworzone pod kątem wydruku w formacie A3 lub na stronę internetową, ale nie jest dla nas wyzwaniem stworzyć znacznie większy obraz, jeśli jest taka potrzeba.
         Nie czekaj, aż ktoś inny wykorzysta okazję, prześlij nam już dziś do wyceny swoje materiały, a otrzymasz ofertę z wyceną już kolejnego dnia roboczego.`
     },
-    // {
-    //     name: 'Wizualizacje wnętrz',
-    //     refName: 'wizInterior',
-    //     img: 'https://www.digitalarch.pl/static/media/93.dade5f43.jpg',
-    //     description: ''
-    // },
+    {
+        name: 'Wizualizacje wnętrz',
+        refName: 'wizInterior',
+        img: INT,
+        description: `lorem ipsum (descritption of the interior to be added soon)`
+    },
     // {
     //     name: 'Wizualizacje deweloperskie',
     //     refName: 'wizDev',
@@ -29,20 +36,20 @@ export const sections = [
     {
         name: 'Wizualizacje produktu',
         refName: 'wizProduct',
-        img: 'https://www.digitalarch.pl/static/media/07.f755ff1c.jpg',
+        img: PRODUCT,
         description: `Potrzebujesz pokazać swój produkt w katalogu, na stronie, czy w ogłoszeniu, a nie jesteś w stanie umieścić jego rzeczywistego zdjęcia? Stworzymy dla Ciebie hiperrealistyczną wizualizację 3d, z zachowaniem nawet najdrobniejszych detali. Bo detal ma ogromne znaczenie. Nieważne czy ma to być projekt szklanki, krzesła, czy samochodu. Zajmiemy się dla Ciebie każdym zadaniem.
         Dzięki technologii będziesz mógł pokazać swój produkt również w wersji interaktywnej, aby klient mógł nim poruszyć, obrócić go, tak jakby miał już go w rękach.`
     },
     {
         name: 'Wirtualne spacery',
         refName: 'vr',
-        img: 'https://www.digitalarch.pl/static/media/53.9074f059.jpg',
+        img: VR,
         description: `Daj swoim klientom możliwość nie tylko zobaczenia „zdjęcia” swojego nowego domu. Daj im wejść do środka już dziś, nawet jeśli nie zostały jeszcze wylane fundamenty. Dodaj im do tego odrobinę najnowszych technologii, a będą mogli dosłownie znaleźć się we wnętrzu swojego przyszłego mieszkania, dzięki zastosowaniu VR w trójwymiarze oraz z pełnym 360 stopniowym zakresem widzenia. `
     },
     {
         name: 'Grafika 3d',
         refName: '3dGraphics',
-        img: 'https://www.digitalarch.pl/static/media/36.5b3b4e3d.jpg',
+        img: GRAPHICS,
         description: `Najbardziej lubimy zadania specjalne, którym nikt inny nie jest w stanie podołać. Stworzymy dla Ciebie unikatową wizytówkę, model do filmu, postać do gry, a nawet stację kosmiczną jeśli tylko tego chcesz. Zaskocz i oczaruj swoich klientów, ukazując im coś, czego nie ma jeszcze konkurencja. Będziesz dzięki temu zawsze krok do przodu.`
     },
 ]

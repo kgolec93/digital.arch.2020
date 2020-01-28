7658b70e3e61c95e2faf49b6a4b2f31cc7c76049
kgolec93 <kamil.golec@gmail.com>
2020-01-28 13:00:34 +0100

    Parallax installed to the jumbo headers

--- src/components/Elements/Banner.js
+++ src/components/Elements/Banner.js
@@ -1,43 +1,57 @@
 import React, { Component } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
 import './Banner.scss';
+import { Parallax, Background } from 'react-parallax'
 
 export class Banner extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            isVisible: true
+        this.state = {
+            isVisible: true,
+            parallaxStr: 200
         }
     }
 
     componentDidMount() {
-        window.addEventListener('scroll', this.listenToScroll)
+        window.addEventListener('scroll', this.listenToScroll);
+        if (window.innerWidth < 700) {
+            this.setState({parallaxStr: 0})
+        }
+
     }
 
     listenToScroll = () => {
-        if (window.pageYOffset >= window.innerHeight-68 ) {
+        if (window.pageYOffset >= window.innerHeight - 68) {
             this.setState({ isVisible: false })
         }
     }
 
     scrollFunc = () => {
-        scroll.scrollTo(this.refs.banner.offsetHeight-68, {
+        scroll.scrollTo(this.refs.banner.offsetHeight - 68, {
             duration: 1000,
             delay: 0,
             smooth: true,
-            offset: -68, // Scrolls to element + 50 pixels down the page
-          })
+            offset: -68
+        })
     }
 
     render() {
         return (
-            <div className='bannerContainer' ref='banner'>
-                <div className="bannerHeader">
-                    <h1>{this.props.section}</h1>
-                    <div onClick={this.scrollFunc}>{this.props.button}</div>
+            <Parallax
+                strength={this.state.parallaxStr}
+            >
+                <div className='bannerContainer' ref='banner'>
+                    <div className="bannerHeader">
+                        <h1>{this.props.section}</h1>
+                        <div onClick={this.scrollFunc}>{this.props.button}</div>
+                    </div>
+
                 </div>
-                <img onLoad={this.props.bannerLoaded} src={this.props.bgImage} alt="wizualizacja-archi"/>
-            </div>
+                <Background className='parallaxBG'>
+                    <img onLoad={this.props.bannerLoaded} src={this.props.bgImage} alt="wizualizacja-archi" />
+                </Background>
+            </Parallax>
+
         )
     }
 }
